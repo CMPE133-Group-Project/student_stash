072628eed6733324dacd5ff50ba7c8ccef364416
Jayce Lee <jayce.sy.lee@gmail.com>
2023-09-19 18:57:39 -0700

    Restructured main.dart to separate into different functions, and changed the BottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,102 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
 
-    return MaterialApp(
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
-      home: Scaffold(
+  @override
+  State<HomePage> createState() =>
+      _HomePageState();
+}
 
-        appBar: AppBar(
+class _HomePageState
+    extends State<HomePage> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Shop',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Search',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Sell',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 4: Chat',
+      style: optionStyle,
+    ),
+  ];
 
-          centerTitle: true,
-          backgroundColor: Colors.indigo,
-          title: const Text('STUDENT STASH'),
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
-          leading: GestureDetector(
-            onTap: () {},
-            child: Icon(Icons.menu,),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('STUDENT STASH'),
+        backgroundColor: Colors.indigo,
+        centerTitle: true,
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Colors.indigo,
           ),
-
-          actions: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(right: 20.0),
-              child: GestureDetector(
-                onTap: () {},
-                child: Icon( Icons.person, ),
-              )
-            )
-          ]
-
-        ),
-
-        bottomNavigationBar: BottomNavigationBar(
-
-          items: const <BottomNavigationBarItem>[
-
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-
-            BottomNavigationBarItem(
-              icon: Icon(Icons.shop),
-              label: 'Shop',
-            ),
-
-            BottomNavigationBarItem(
-              icon: Icon(Icons.search),
-              label: 'Search',
-            ),
-
-            BottomNavigationBarItem(
-              icon: Icon(Icons.sell),
-              label: 'Sell',
-            ),
-
-            BottomNavigationBarItem(
-              icon: Icon(Icons.chat),
-              label: 'Chat',
-            ),
-
-          ],
-
-          type: BottomNavigationBarType.fixed,
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          backgroundColor: Colors.indigo,
-        ),
-
-        body: Column(
-          children: [
-            const Center(
-              child: Padding(
-                child: Text('Hello'),
-                padding: EdgeInsets.all(10),
-              )
-
-            )
-          ]
-        ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shop),
+            label: 'Shop',
+            backgroundColor: Colors.indigo,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+            backgroundColor: Colors.indigo,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.sell),
+            label: 'Sell',
+            backgroundColor: Colors.indigo,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Chat',
+            backgroundColor: Colors.indigo,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.white,
+        onTap: _onItemTapped,
       ),
     );
   }
